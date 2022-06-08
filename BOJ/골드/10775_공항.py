e610fecb34154560e5af3992c1d3be9196b0648b
강민재 <kmj5052@gmail.com>
2022-06-08 18:00:06 +0900

    백준 골드문제 10문제 풀이

--- BOJ/골드/10775_공항.py
+++ BOJ/골드/10775_공항.py
@@ -1,6 +1,32 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**4)
+
+
+def find(a):
+    if air_port[a] != a:
+        air_port[a] = find(air_port[a])
+    return air_port[a]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a > b:
+        air_port[a] = b
+    else:
+        air_port[b] = a
+
+
 G = int(input())
 P = int(input())
-dj_set = set()
+air_port = {i: i for i in range(G+1)}
+ans = 0
 for _ in range(P):
-    dj_set.add(input())
-print(len(dj_set))
+    g = int(input())
+    g = find(g)
+    if g == 0:
+        break
+    union(g, g-1)
+    ans += 1
+print(ans)
--- BOJ/골드/1647_도시분할계획.py
+++ BOJ/골드/1647_도시분할계획.py
@@ -1,14 +1,36 @@
-from collections import defaultdict
-import heapq
+import sys
+input = sys.stdin.readline
 
 
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
 
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
 
 
 N, M = map(int, input().split())
-adj_dict = defaultdict(list)
+que = []
+ans = 0
+last_c = 0
+parent = [i for i in range(N+1)]
 for _ in range(M):
     a, b, c = map(int, input().split())
-    adj_dict[a].append((b, c))
-    adj_dict[b].append((a, c))
-print(prim())
+    que.append((c, a, b))
+
+que.sort(reverse=True)
+while que:
+    c, a, b = que.pop()
+    if find(a) != find(b):
+        union(a, b)
+        ans += c
+        last_c = c
+print(ans-last_c)
